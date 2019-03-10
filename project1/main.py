cc7a26c29b2721b05d90a6986b1290fd08ff0dba
soonerfan237 <soonerfan237@gmail.com>
2019-03-09 18:21:43 -0600

    Created travis file. Set up command line arguments.

---
+++ project1/main.py
@@ -0,0 +1,19 @@
+import argparse
+
+def main(input):
+    print(input)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, required=True,help="File extension to be read.")
+    parser.add_argument("--names", type=str,help="Redact names.")
+    parser.add_argument("--dates", type=str,help="Redact dates.")
+    parser.add_argument("--addressess", type=str,help="Redact addresses.")
+    parser.add_argument("--phones", type=str,help="Redact phone numbers.")
+    parser.add_argument("--concept", type=str,help="Redact concept.")
+    parser.add_argument("--output", type=str,help="Output location.")
+    parser.add_argument("--stats", type=str,help="Redaction stats.")    
+    args = parser.parse_args()
+    if args.input:
+        main(args.input)
