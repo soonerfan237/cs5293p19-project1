366346a1205119311121b43d75b3d9576a15a50f
soonerfan237 <soonerfan237@gmail.com>
2019-03-09 19:54:18 -0600

    Added function to write out files with .redacted suffix.

--- project1/main.py
+++ project1/main.py
@@ -1,17 +1,41 @@
-import argparse
+import argparse #for parsing command line arguments
 import nltk
-import os
-import glob
+import os #for checking if file or directory exists
+import glob #for finding files in directory
+import shutil #for copying files
 
-def main(inputs):
-    #cwd = os.getcwd()
-    #for file in os.listdir(cwd):
-    #    if file.endswith():
-    #        print(os.path.join(cwd, file))
-    #print(input)
-    for input in inputs:
+#inputfiles = []
+
+def main(args_input, args_output):
+    input_files = inputfiles(args_input)
+    outputfiles(input_files,args_output)
+
+def inputfiles(args_input):
+    input_files = []
+    for input in args_input:
         for file in glob.glob(input):
-            print(file)
+            #print(file)
+            input_files.append(file)
+    return input_files
+
+def outputfiles(input_files, args_output):
+    cwd = os.getcwd()
+    output_directory = cwd + '/' + args_output
+    if not os.path.exists(output_directory):
+        print("directory NOT exist")
+        os.mkdir(output_directory)
+    else:
+        print("directory exists")
+    for file in input_files:
+        original_file_path = cwd + '/' + file
+        redacted_file_name = os.path.basename(original_file_path) + ".redacted"
+        redacted_file_path = output_directory + redacted_file_name
+        #print("redacted_file_path = " + redacted_file_path)
+        if os.path.exists(original_file_path):
+            #print("file exists")
+            shutil.copy(original_file_path, redacted_file_path)
+        else:
+            print("file NOT exist: " + cwd + '/' + file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,4 +49,4 @@ if __name__ == '__main__':
     parser.add_argument("--stats", type=str,help="Redaction stats.")    
     args = parser.parse_args()
     if args.input:
-        main(args.input)
+        main(args.input, args.output)
