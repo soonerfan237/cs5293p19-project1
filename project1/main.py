201c061aa4653f4c7cc350a0408e9ac7f899085d
soonerfan237 <soonerfan237@gmail.com>
2019-03-09 19:14:05 -0600

    Finding files based on input command line arguments.

--- project1/main.py
+++ project1/main.py
@@ -1,13 +1,21 @@
 import argparse
 import nltk
+import os
+import glob
 
-def main(input):
-    print(input)
-
+def main(inputs):
+    #cwd = os.getcwd()
+    #for file in os.listdir(cwd):
+    #    if file.endswith():
+    #        print(os.path.join(cwd, file))
+    #print(input)
+    for input in inputs:
+        for file in glob.glob(input):
+            print(file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=True,help="File extension to be read.")
+    parser.add_argument("--input", type=str, action="append",required=True,help="File extension to be read.")
     parser.add_argument("--names", type=str,help="Redact names.")
     parser.add_argument("--dates", type=str,help="Redact dates.")
     parser.add_argument("--addressess", type=str,help="Redact addresses.")
